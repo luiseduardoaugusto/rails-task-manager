75d8bf4d1e46010cdcbdd9fb9e5beb14ce76314f
luis augusto <luis.augusto.u@gmail.com>
2022-01-18 18:52:09 -0500

    ok 34

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'task#index'
+
+  get 'tasks/:id', to: 'task#show', as: :task
+
+  get 'new', to: 'task#new'
+
+  post 'tasks', to: 'task#create'
+
+  get 'tasks/:id/edit', to: 'task#edit', as: :edit
+
+  patch 'tasks/:id', to: 'task#update'
+
+  delete "tasks/:id", to: "task#destroy"
+
 end
